4fd8d3765da209d619b6761eabddaed14895b334
soaprt <sergey.ostrovsky.prt@gmail.com>
2024-06-25 19:29:29 +0200

    Setup connect to database

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,10 @@ dependencies {
     implementation("io.ktor:ktor-server-cio:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
+    // Database
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-
     implementation("org.postgresql:postgresql:42.2.2")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/main/kotlin/com/tradume/Application.kt
+++ src/main/kotlin/com/tradume/Application.kt
@@ -4,8 +4,16 @@ import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 import com.tradume.plugins.*
 import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
+    Database.connect(
+        url = "jdbc:postgresql://localhost:5432/tradume",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "nU5sP8Zc"
+    )
+
     embeddedServer(
         CIO,
         port = 8080,
